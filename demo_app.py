6414f6adddffd977b040d8134c97f887e459dbd7
Panagiotis Meletis <p_c_meletis@yahoo.com>
2024-06-16 01:22:35 +0200

    Add two figures to the app and add BTC index.

--- demo_app.py
+++ demo_app.py
@@ -7,90 +7,202 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.graph_objs as go
+import plotly.express as px
 
-from funclib import (days_since_ath, download_and_save_data,
-                     get_latest_close_data)
+from funclib import days_since_ath, get_latest_close_data
 
-# import pandas as pd
-# import yfinance as yf
-# import mpld3
-# import streamlit.components.v1 as components
 
 DIRPATH_DATASET = Path.absolute(Path('.')) / 'datasets'
 
 
 st.title('Insights on Financial Markets')
 
-data_load_state = st.text('Loading data...')
+intro_str = """A collection of insights and analytics on the US stock markets.
+
+You can find the source for this website at https://github.com/pmeletis/financial-assets-insights.
+"""
+st.write(intro_str)
+
+daily_close_df = get_latest_close_data(DIRPATH_DATASET)
+
+###############################################################################
 
 st.header('Indices')
 
-daily_close = get_latest_close_data(DIRPATH_DATASET)
+data_load_state = st.text('Creating graph...')
 
-data_load_state.text('Loading data... done!')
+daily_close = daily_close_df.copy()
+daily_close['Date'] = daily_close.index
+# Melt the dataframe for Plotly Express
+melted_data = daily_close.melt(id_vars='Date', var_name='index', value_name='USD')
 
-data_load_state.text('Creating graphs...')
+fig = px.line(data_frame=melted_data, x='Date', y='USD', color='index', 
+              # labels={'value': 'Value', 'date': 'Date', 'series': 'Series'},
+              # title='Time Series Data',
+              log_y=True,
+)
 
-col1, col2, col3 = st.columns(3)
-sp_check = col1.checkbox('S&P500', value=True)
-nc_check = col2.checkbox('NASDAQ Comp', value=True)
-r2_check = col3.checkbox('RUSSEL2000', value=True)
+start_year = daily_close.index.year.min()
+end_year = daily_close.index.year.max()
+years = list(range(1930, end_year + 1, 5))
+if start_year < 1930:
+  years.insert(0, start_year)
+if years[-1] != end_year:
+  years.append(end_year)
 
-colors = dict(zip(daily_close.columns, plt.cm.tab10.colors))
-fig = plt.figure(figsize=(14, 7), dpi=300)
-if sp_check:
-  plt.plot(daily_close['S&P500'], color=colors['S&P500'], label='S&P500')
-  plt.hlines(daily_close['S&P500'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['S&P500'], linestyles='--', alpha=0.3, zorder=-1)
-if nc_check:
-  plt.plot(daily_close['NASDAQ Comp'], color=colors['NASDAQ Comp'], label='NASDAQ Comp')
-  plt.hlines(daily_close['NASDAQ Comp'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['NASDAQ Comp'], linestyles='--', alpha=0.3, zorder=-1)
-if r2_check:
-  plt.plot(daily_close['RUSSEL2000'], color=colors['RUSSEL2000'], label='RUSSEL2000')
-  plt.hlines(daily_close['RUSSEL2000'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['RUSSEL2000'], linestyles='--', alpha=0.3, zorder=-1)
-plt.yscale('log')
-plt.xlabel('Date')
-plt.xlim(left=daily_close.index[0], right=daily_close.index[-1])
-plt.tick_params(axis='y', which='both', right=True, labelright=True)
-plt.tight_layout()
-plt.legend()
-st.pyplot(fig=fig)
+fig.update_layout(
+    # autosize=True,
+    # xaxis=dict(
+    #     tickformat='%Y',  # Show year only
+    #     dtick="M60",  # Tick every 60 months (5 years)
+    #     tickangle=45  # Rotate labels for better readability
+    # ),
+    xaxis=dict(
+        tickmode='array',
+        tickvals=pd.to_datetime([f'{year}-01-01' for year in years]),
+        ticktext=[str(year) for year in years],
+        tickangle=45,  # Rotate labels for better readability
+        range=[pd.to_datetime(f'{start_year}-01-01'), pd.to_datetime(f'{end_year}-12-31')],
+    ),
+    # yaxis=dict(
+    #     minor=dict(showgrid=False)
+    # ),
+    # height=500  # you can adjust the height if needed
+)
+
+# fig.update_xaxes(autorange=True)
+
+data_load_state.text('Creating graph... Done!')
+st.plotly_chart(fig, use_container_width=True)
+data_load_state.empty()
+
+############################################################################
+
+st.header('Days since latest All Time High')
+
+data_load_state = st.text('Creating graph...')
+
+dsath = daily_close_df.copy()
+dsath = dsath.apply(days_since_ath)
+
+dsath['Date'] = dsath.index
+# Melt the dataframe for Plotly Express
+melted_data = dsath.melt(id_vars='Date', var_name='index', value_name='# days')
+fig = px.line(data_frame=melted_data, x='Date', y='# days', color='index', 
+              # labels={'value': 'Value', 'date': 'Date', 'series': 'Series'},
+              # title='Time Series Data',
+              # log_y=True,
+)
+fig.update_yaxes(title='Days since latest ATH')
+
+fig.update_layout(
+    xaxis=dict(
+        tickmode='array',
+        tickvals=pd.to_datetime([f'{year}-01-01' for year in years]),
+        ticktext=[str(year) for year in years],
+        tickangle=45,  # Rotate labels for better readability
+        range=[pd.to_datetime(f'{start_year}-01-01'), pd.to_datetime(f'{end_year}-12-31')],
+    ),
+)
+
+data_load_state.text('Creating graph... Done!')
+st.plotly_chart(fig, use_container_width=True)
+data_load_state.empty()
+
+##########################################################################################
+
+# dsath_sp = dsath['S&P500']
+# dsath_nc = dsath['NASDAQ Comp']
+# dsath_r2 = dsath['RUSSEL2000']
+# dsath_btc = dsath['BTCUSD']
+# fig, _ = plt.subplots(ncols=2, figsize=(12,5), width_ratios=(3, 1))
+# plt.subplot(1, 2, 1)
+# plt.plot(dsath['S&P500'], label='S&P500')
+# plt.hlines(dsath_sp.iloc[-1], dsath_sp.index[0], dsath_sp.index[-1], linestyles='--', alpha=0.3, label='S&P 500 days since ATH')
+# plt.plot(dsath_nc, alpha=0.7, label='NASDAQ Composite')
+# plt.plot(dsath_r2, alpha=0.5, label='Russel 2000')
+# plt.plot(dsath_btc, alpha=0.5, label='BTC')
+# plt.gca().xaxis.set_major_locator(YearLocator(base=5))
+# plt.ylabel('Days since latest ATH')
+# plt.xlabel('Date')
+# plt.legend()
 
 
-dsath =  daily_close.apply(days_since_ath)
-dsath_sp = dsath['S&P500']
-dsath_nc = dsath['NASDAQ Comp']
-dsath_r2 = dsath['RUSSEL2000']
-top_limit = max(*dsath_sp[-2*365:], *dsath_nc[-2*365:], *dsath_r2[-2*365:])
+# col1, col2, col3 = st.columns(3)
+# sp_check = col1.checkbox('S&P500', value=True)
+# nc_check = col2.checkbox('NASDAQ Comp', value=True)
+# r2_check = col3.checkbox('RUSSEL2000', value=True)
 
-fig, _ = plt.subplots(ncols=2, figsize=(12,5), width_ratios=(3, 1))
-plt.subplot(1, 2, 1)
-plt.plot(dsath['S&P500'], label='S&P500')
-plt.hlines(dsath_sp.iloc[-1], dsath_sp.index[0], dsath_sp.index[-1], linestyles='--', alpha=0.3, label='S&P 500 days since ATH')
-plt.plot(dsath_nc, alpha=0.6, label='NASDAQ Composite')
-plt.plot(dsath_r2, alpha=0.3, label='Russel 2000')
-plt.ylabel('Days since latest ATH')
-plt.xlabel('Date')
-plt.legend()
+# fig = go.Figure()
 
-# rect = patches.Rectangle((plt.xlim()[1]-100, 0), 40, top_limit, linewidth=1, edgecolor='r', facecolor='none')
-# plt.gca().add_patch(rect)
-plt.subplot(1, 2, 2)
-plt.plot(dsath['S&P500'], label='S&P500')
-plt.hlines(dsath_sp.iloc[-1], dsath_sp.index[0], dsath_sp.index[-1], linestyles='--', alpha=0.3, label='S&P 500 days since ATH')
-plt.plot(dsath_nc, alpha=0.6, label='NASDAQ Composite')
-plt.plot(dsath_r2, alpha=0.3, label='Russel 2000')
-plt.ylabel('Days since latest ATH')
-plt.xlabel('Date')
-plt.xlim(dsath_sp.index[-2*365], dsath_sp.index[-1])
-plt.ylim(0, top_limit)
-plt.gcf().autofmt_xdate()
-plt.tight_layout()
-# Draw an arrow from the first subplot to the second subplot
-arrow = plt.Arrow(0.7, 0.25, 0.06, 0, transform=fig.transFigure, color='k', width=0.03)
-fig.add_artist(arrow)
-st.pyplot(fig=fig)
+# if sp_check:
+#     fig.add_trace(go.Scatter(x=data['date'], y=data['series1'], mode='lines', name='Series 1'))
+# if show_series2:
+#     fig.add_trace(go.Scatter(x=data['date'], y=data['series2'], mode='lines', name='Series 2'))
+# if show_series3:
+#     fig.add_trace(go.Scatter(x=data['date'], y=data['series3'], mode='lines', name='Series 3'))
 
-data_load_state.text('Creating graphs... done!')
+# Update layout
+# fig.update_layout(title='Time Series Data', xaxis_title='Date', yaxis_title='Value')
+
+# Display the plot in Streamlit
+# st.plotly_chart(fig)
+
+# colors = dict(zip(daily_close.columns, plt.cm.tab10.colors))
+# fig = plt.figure(figsize=(14, 7), dpi=300)
+# if sp_check:
+#   plt.plot(daily_close['S&P500'], color=colors['S&P500'], label='S&P500')
+#   plt.hlines(daily_close['S&P500'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['S&P500'], linestyles='--', alpha=0.3, zorder=-1)
+# if nc_check:
+#   plt.plot(daily_close['NASDAQ Comp'], color=colors['NASDAQ Comp'], label='NASDAQ Comp')
+#   plt.hlines(daily_close['NASDAQ Comp'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['NASDAQ Comp'], linestyles='--', alpha=0.3, zorder=-1)
+# if r2_check:
+#   plt.plot(daily_close['RUSSEL2000'], color=colors['RUSSEL2000'], label='RUSSEL2000')
+#   plt.hlines(daily_close['RUSSEL2000'].iloc[-1], daily_close.index[0], daily_close.index[-1], colors=colors['RUSSEL2000'], linestyles='--', alpha=0.3, zorder=-1)
+# plt.yscale('log')
+# plt.xlabel('Date')
+# plt.xlim(left=daily_close.index[0], right=daily_close.index[-1])
+# plt.tick_params(axis='y', which='both', right=True, labelright=True)
+# plt.tight_layout()
+# plt.legend()
+# st.pyplot(fig=fig)
+
+
+# dsath =  daily_close.apply(days_since_ath)
+# dsath_sp = dsath['S&P500']
+# dsath_nc = dsath['NASDAQ Comp']
+# dsath_r2 = dsath['RUSSEL2000']
+# top_limit = max(*dsath_sp[-2*365:], *dsath_nc[-2*365:], *dsath_r2[-2*365:])
+
+# fig, _ = plt.subplots(ncols=2, figsize=(12,5), width_ratios=(3, 1))
+# plt.subplot(1, 2, 1)
+# plt.plot(dsath['S&P500'], label='S&P500')
+# plt.hlines(dsath_sp.iloc[-1], dsath_sp.index[0], dsath_sp.index[-1], linestyles='--', alpha=0.3, label='S&P 500 days since ATH')
+# plt.plot(dsath_nc, alpha=0.6, label='NASDAQ Composite')
+# plt.plot(dsath_r2, alpha=0.3, label='Russel 2000')
+# plt.ylabel('Days since latest ATH')
+# plt.xlabel('Date')
+# plt.legend()
+
+# # rect = patches.Rectangle((plt.xlim()[1]-100, 0), 40, top_limit, linewidth=1, edgecolor='r', facecolor='none')
+# # plt.gca().add_patch(rect)
+# plt.subplot(1, 2, 2)
+# plt.plot(dsath['S&P500'], label='S&P500')
+# plt.hlines(dsath_sp.iloc[-1], dsath_sp.index[0], dsath_sp.index[-1], linestyles='--', alpha=0.3, label='S&P 500 days since ATH')
+# plt.plot(dsath_nc, alpha=0.6, label='NASDAQ Composite')
+# plt.plot(dsath_r2, alpha=0.3, label='Russel 2000')
+# plt.ylabel('Days since latest ATH')
+# plt.xlabel('Date')
+# plt.xlim(dsath_sp.index[-2*365], dsath_sp.index[-1])
+# plt.ylim(0, top_limit)
+# plt.gcf().autofmt_xdate()
+# plt.tight_layout()
+# # Draw an arrow from the first subplot to the second subplot
+# arrow = plt.Arrow(0.7, 0.25, 0.06, 0, transform=fig.transFigure, color='k', width=0.03)
+# fig.add_artist(arrow)
+# st.pyplot(fig=fig)
+
 
 # Some number in the range 0-23
 # hour_to_filter = st.slider('hour', 0, 23, 17)
--- funclib.py
+++ funclib.py
@@ -21,15 +21,18 @@ def download_and_save_data(dirpath: Path):
   # fetch data with maximum duration
   hist = yf.download('^IXIC', start='1927-01-01') # '^IXIC' for Nasdaq Composite
   hist.to_csv(dirpath / f'nasdaq_comp_daily_{current_date}.csv')
-  time.sleep(1)
+  time.sleep(2)
   hist = yf.download('^GSPC', start='1927-01-01') # ^GSPC' for S&P 500
   hist.to_csv(dirpath / f's&p500_daily_{current_date}.csv')
-  time.sleep(1)
+  time.sleep(2)
   hist = yf.download('^RUT', start='1927-01-01') # for Russel 2000
   hist.to_csv(dirpath / f'russel2000_daily_{current_date}.csv')
+  time.sleep(2)
+  hist = yf.download('BTC-USD', start='1927-01-01') # for Bitcoin
+  hist.to_csv(dirpath / f'btcusd_daily_{current_date}.csv')
 
 
-def _get_most_recent(dirpath, prefix):
+def _get_most_recent(dirpath: Path, prefix):
   filepaths = dirpath.glob(f'{prefix}_*.csv')
   dates = [fp.name[-12:-4] for fp in filepaths]
   if dates:
@@ -45,30 +48,38 @@ def get_latest_close_data(dirpath: Path = Path()):
   reader_fn = partial(pd.read_csv, parse_dates=['Date'], index_col='Date')
 
   filepath_sp = _get_most_recent(dirpath, 's&p500_daily')
-  url_sp = URL_ASSETS + 's%26p500_daily_20240208.csv'
+  url_sp = URL_ASSETS + 's%26p500_daily_20240322.csv'
   if filepath_sp is not None:
     sp500_daily = reader_fn(filepath_sp)
   else:
     sp500_daily = reader_fn(url_sp)
 
   filepath_nc = _get_most_recent(dirpath, 'nasdaq_comp_daily')
-  url_nc = URL_ASSETS + 'nasdaq_comp_daily_20240208.csv'
+  url_nc = URL_ASSETS + 'nasdaq_comp_daily_20240322.csv'
   if filepath_nc is not None:
     nasdaq_comp_daily = reader_fn(filepath_nc)
   else:
     nasdaq_comp_daily = reader_fn(url_nc)
 
   filepath_r2 = _get_most_recent(dirpath, 'russel2000_daily')
-  url_r2 = URL_ASSETS + 'russel2000_daily_20240208.csv'
+  url_r2 = URL_ASSETS + 'russel2000_daily_20240322.csv'
   if filepath_nc is not None:
     russel2000_daily = reader_fn(filepath_r2)
   else:
     russel2000_daily = reader_fn(url_r2)
 
+  filepath_btc = _get_most_recent(dirpath, 'btcusd_daily')
+  url_btc = URL_ASSETS + 'btcusd_daily_20240322.csv'
+  if filepath_nc is not None:
+    btcusd_daily = reader_fn(filepath_btc)
+  else:
+    btcusd_daily = reader_fn(url_btc)
+
   # aggregate data
   indices_all_daily_close = pd.DataFrame({'S&P500': sp500_daily['Close'],
                                           'NASDAQ Comp': nasdaq_comp_daily['Close'],
-                                          'RUSSEL2000': russel2000_daily['Close']})
+                                          'RUSSEL2000': russel2000_daily['Close'],
+                                          'BTCUSD': btcusd_daily['Close']})
   # delete latest day to be sure all indices have no-NaN value
   indices_all_daily_close = indices_all_daily_close[:-1]
 
@@ -78,7 +89,9 @@ def get_latest_close_data(dirpath: Path = Path()):
 def days_since_ath(signal: pd.Series, eps: Optional[float] = None):
   # get rid of any NaNs in the beginning
   signal = signal[signal.first_valid_index():]
-  assert not signal.isna().any()
+  # it can be that S&P does not have values for the weekend days
+  signal = signal.ffill()
+  # assert not signal.isna().any(), (signal.name, signal.first_valid_index())
   assert eps is None or (isinstance(eps, float) and eps >= 0.0)
   ath = signal.iloc[0]
   num_days_since_ath = [0]
